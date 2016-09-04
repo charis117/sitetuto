fb575cb803698ba8318c2cee44057e0225f5c665
Chris <manyac4@gmail.com>
2016-09-04 03:59:06 +0300

    Update myprocessingscript.php

--- myprocessingscript.php
+++ myprocessingscript.php
@@ -1,11 +1,14 @@
 <?php
 $txt = "data.txt"; 
 $fh = fopen($txt, 'w+'); 
+
 if (isset($_POST['field1']) && isset($_POST['field2'])) { // check if both fields are set
    $txt=$_POST['field1'].' - '.$_POST['field2']; 
+   echo("Recieved"+$_POST['field1']);
    file_put_contents('data.txt',$txt."\n",FILE_APPEND); // log to data.txt 
    exit();
 }
     fwrite($fh,$txt); // Write information to the file
     fclose($fh); // Close the file
+    echo("ALl good!");
     ?>
